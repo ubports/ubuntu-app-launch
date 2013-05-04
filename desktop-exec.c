b2ec1961252d85a1a6edcbb4fcdcc423707e7922
Ted Gould <ted@gould.cx>
2013-05-03 17:47:11 -0700

    Fleshing out desktop exec

--- desktop-exec.c
+++ desktop-exec.c
@@ -5,7 +5,22 @@
 int
 main (int argc, char * argv[])
 {
+	if (argc != 2) {
+		g_error("Should be called as: %s <app_id>", argv[0]);
+		return 1;
+	}
 
+	gchar * desktop = g_strdup_printf("%s.desktop", argv[1]);
+	GDesktopAppInfo * appinfo = g_desktop_app_info_new(desktop);
+	g_free(desktop);
 
+	if (!G_IS_DESKTOP_APP_INFO(appinfo)) {
+		g_error("Unable to load app '%s'", argv[1]);
+		return 1;
+	}
+
+	g_app_info_launch(G_APP_INFO(appinfo), NULL, NULL, NULL);
+
+	g_object_unref(appinfo);
 	return 0;
 }
