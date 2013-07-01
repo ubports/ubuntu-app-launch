ad0c001a7531276746089e5d193d375a6869efab
Ted Gould <ted@gould.cx>
2013-07-01 15:11:05 -0500

    No 'r'

--- test-conffile.sh
+++ test-conffile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -x /sbin/initctrl.REAL ] ; then
-	init-checkconf --initctl-path=/sbin/initctrl.REAL $1
+if [ -x /sbin/initctl.REAL ] ; then
+	init-checkconf --initctl-path=/sbin/initctl.REAL $1
 else
 	init-checkconf $1
 fi
