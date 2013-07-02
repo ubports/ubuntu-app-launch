06fe910be1d671cdccdb99e06ef623506e5dc9b5
Ted Gould <ted@gould.cx>
2013-07-02 12:17:08 -0500

    Flipping around the test and just printing debug info if we fail

--- test-conffile.sh
+++ test-conffile.sh
@@ -2,9 +2,11 @@
 
 UPSTARTVERSION=`initctl version | sed 's/[[:alpha:]\)|(|[:space:]]//g' | awk -F- '{print $1}' | awk -F. '{print $2}'`
 
-if [ ${UPSTARTVERSION} -lt 8 ] ; then
-# Old upstart on builders, don't test
-	true
-else
+# Only test on newer versions of Upstart, like not the
+# versions on the builders
+if [ ${UPSTARTVERSION} -gt 7 ] ; then
 	init-checkconf $1
+else
+	echo "Upstart Version: $UPSTARTVERSION"
+	echo "  ....Skipping Tests"
 fi
