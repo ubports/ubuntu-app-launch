923affeadf270830b18b733cab49b6329d60a47c
Ted Gould <ted@gould.cx>
2013-07-31 22:24:19 -0500

    Switch to using GDBus and calling the function directly

--- libupstart-app-launch/upstart-app-launch.c
+++ libupstart-app-launch/upstart-app-launch.c
@@ -1,84 +1,71 @@
 
 #include "upstart-app-launch.h"
-#include <upstart.h>
-#include <nih/alloc.h>
-
-static NihDBusProxy *
-nih_proxy_create (void)
-{
-	DBusError        error;
-	DBusConnection * conn;
-	NihDBusProxy *   upstart;
-	const gchar *    upstart_session;
-
-	upstart_session = g_getenv("UPSTART_SESSION");
-	if (upstart_session == NULL) {
-		g_warning("Not running under Upstart User Session");
-		return NULL;
-	}
-
-	dbus_error_init(&error);
-	conn = dbus_connection_open_private(upstart_session, &error);
-
-	if (conn == NULL) {
-		g_warning("Unable to connect to the Upstart Session: %s", error.message);
-		dbus_error_free(&error);
-		return NULL;
-	}
-
-	dbus_error_free(&error);
-
-	upstart = nih_dbus_proxy_new(NULL, conn,
-		DBUS_SERVICE_UPSTART,
-		DBUS_PATH_UPSTART,
-		NULL, NULL);
-
-	if (upstart == NULL) {
-		g_warning("Unable to build proxy to Upstart");
-		dbus_connection_close(conn);
-		dbus_connection_unref(conn);
-		return NULL;
-	}
-
-	dbus_connection_unref(conn);
-
-	upstart->auto_start = FALSE;
-
-	return upstart;
-}
+#include <gio/gio.h>
 
 gboolean
 upstart_app_launch_start_application (const gchar * appid, const gchar * const * uris)
 {
-	NihDBusProxy * proxy = nih_proxy_create();
-	if (proxy == NULL) {
+	const gchar * upstart_session = g_getenv("UPSTART_SESSION");
+	if (upstart_session == NULL) {
+		g_warning("Upstart User Session Not Found");
 		return FALSE;
 	}
 
+	GError * error = NULL;
+	GDBusConnection * upstart = g_dbus_connection_new_for_address_sync(upstart_session,
+	                                                                   G_DBUS_CONNECTION_FLAGS_AUTHENTICATION_CLIENT,
+	                                                                   NULL, /* auth */
+	                                                                   NULL, /* cancel */
+	                                                                   &error);
+	if (error != NULL) {
+		g_warning("Unable to connect to Upstart bus: %s", error->message);
+		g_error_free(error);
+		return FALSE;
+	}
+
+	GVariantBuilder builder;
+	g_variant_builder_init(&builder, G_VARIANT_TYPE_TUPLE);
+
+	g_variant_builder_add_value(&builder, g_variant_new_string("application-start"));
+
+	g_variant_builder_open(&builder, G_VARIANT_TYPE_ARRAY);
+
 	gchar * env_appid = g_strdup_printf("APP_ID=%s", appid);
+	g_variant_builder_add_value(&builder, g_variant_new_take_string(env_appid));
+
 	gchar * env_uris = NULL;
 
 	if (uris != NULL) {
 		gchar * urisjoin = g_strjoinv(" ", (gchar **)uris);
 		env_uris = g_strdup_printf("APP_URIS=%s", urisjoin);
 		g_free(urisjoin);
+
+		g_variant_builder_add_value(&builder, g_variant_new_take_string(env_uris));
 	}
 
-	gchar * env[3] = {
-		env_appid,
-		env_uris,
-		NULL
-	};
+	g_variant_builder_close(&builder);
+	g_variant_builder_add_value(&builder, g_variant_new_boolean(FALSE));
+
+	g_dbus_connection_call_sync(upstart,
+	                            "com.ubuntu.Upstart",
+	                            "/com/ubuntu/Upstart",
+	                            "com.ubuntu.Upstart0_6",
+	                            "EmitEvent",
+	                            g_variant_builder_end(&builder),
+	                            NULL,
+	                            G_DBUS_CALL_FLAGS_NONE,
+	                            -1,
+	                            NULL,
+	                            &error);
 
 	gboolean retval = TRUE;
-	if (upstart_emit_event_sync(NULL, proxy, "application-start", env, 100) != 0) {
-		g_warning("Unable to emit signal 'application-start'");
+	if (error != NULL) {
+		g_error("Unable to emit event: %s", error->message);
+		g_error_free(error);
 		retval = FALSE;
 	}
 
-	g_free(env_appid);
-	g_free(env_uris);
-	nih_unref(proxy, NULL);
+	g_object_unref(upstart);
 
 	return retval;
 }
