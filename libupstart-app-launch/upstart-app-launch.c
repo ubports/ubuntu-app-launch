7cc2c7d0b1c27d37079ff3abddf51ba4ce415ac2
Ted Gould <ted@gould.cx>
2013-07-31 21:23:15 -0500

    Fleshing out emitting the signal to start an application

--- libupstart-app-launch/upstart-app-launch.c
+++ libupstart-app-launch/upstart-app-launch.c
@@ -1,11 +1,84 @@
 
 #include "upstart-app-launch.h"
+#include <upstart.h>
+#include <nih/alloc.h>
+
+static NihDBusProxy *
+nih_proxy_create (void)
+{
+	DBusError        error;
+	DBusConnection * conn;
+	NihDBusProxy *   upstart;
+	const gchar *    upstart_session;
+
+	upstart_session = g_getenv("UPSTART_SESSION");
+	if (upstart_session == NULL) {
+		g_warning("Not running under Upstart User Session");
+		return NULL;
+	}
+
+	dbus_error_init(&error);
+	conn = dbus_connection_open(upstart_session, &error);
+
+	if (conn == NULL) {
+		g_warning("Unable to connect to the Upstart Session: %s", error.message);
+		dbus_error_free(&error);
+		return NULL;
+	}
+
+	dbus_error_free(&error);
+
+	upstart = nih_dbus_proxy_new(NULL, conn,
+		DBUS_SERVICE_UPSTART,
+		DBUS_PATH_UPSTART,
+		NULL, NULL);
+
+	dbus_connection_unref(conn);
+
+	if (upstart == NULL) {
+		g_warning("Unable to build proxy to Upstart");
+		return NULL;
+	}
+
+	upstart->auto_start = FALSE;
+
+	return upstart;
+}
 
 gboolean
 upstart_app_launch_start_application (const gchar * appid, const gchar * const * uris)
 {
+	NihDBusProxy * proxy = nih_proxy_create();
+	if (proxy == NULL) {
+		return FALSE;
+	}
 
-	return FALSE;
+	gchar * env_appid = g_strdup_printf("APP_ID=%s", appid);
+	gchar * env_uris = NULL;
+
+	if (uris != NULL) {
+		gchar * urisjoin = g_strjoinv(" ", (gchar **)uris);
+		env_uris = g_strdup_printf("APP_URIS=%s", urisjoin);
+		g_free(urisjoin);
+	}
+
+	gchar * env[3] = {
+		env_appid,
+		env_uris,
+		NULL
+	};
+
+	gboolean retval = TRUE;
+	if (upstart_emit_event_sync(NULL, proxy, "application-start", env, 100) != 0) {
+		g_warning("Unable to emit signal 'application-start'");
+		retval = FALSE;
+	}
+
+	g_free(env_appid);
+	g_free(env_uris);
+	nih_unref(proxy, NULL);
+
+	return retval;
 }
 
 gboolean
