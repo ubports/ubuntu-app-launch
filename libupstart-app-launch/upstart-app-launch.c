5354059220fc00ecf47e301e55f8a8c8eeccea72
Ted Gould <ted@gould.cx>
2013-08-07 09:31:16 -0500

    Make the code a bit easier to read

--- libupstart-app-launch/upstart-app-launch.c
+++ libupstart-app-launch/upstart-app-launch.c
@@ -23,56 +23,56 @@
 #include <gio/gio.h>
 #include <string.h>
 
-static void
-nih_proxy_create (NihDBusProxy ** upstart, DBusConnection ** conn)
+static NihDBusProxy *
+nih_proxy_create (void)
 {
+	NihDBusProxy *   upstart;
+	DBusConnection * conn;
 	DBusError        error;
 	const gchar *    upstart_session;
 
 	upstart_session = g_getenv("UPSTART_SESSION");
 	if (upstart_session == NULL) {
 		g_warning("Not running under Upstart User Session");
-		return;
+		return NULL;
 	}
 
 	dbus_error_init(&error);
-	*conn = dbus_connection_open(upstart_session, &error);
+	conn = dbus_connection_open(upstart_session, &error);
 
-	if (*conn == NULL) {
+	if (conn == NULL) {
 		g_warning("Unable to connect to the Upstart Session: %s", error.message);
 		dbus_error_free(&error);
-		return;
+		return NULL;
 	}
 
 	dbus_error_free(&error);
 
-	*upstart = nih_dbus_proxy_new(NULL, *conn,
+	upstart = nih_dbus_proxy_new(NULL, conn,
 		NULL,
 		DBUS_PATH_UPSTART,
 		NULL, NULL);
 
-	if (*upstart == NULL) {
+	if (upstart == NULL) {
 		g_warning("Unable to build proxy to Upstart");
-		dbus_connection_close(*conn);
-		dbus_connection_unref(*conn);
-		*conn = NULL;
-		return;
+		dbus_connection_close(conn);
+		dbus_connection_unref(conn);
+		return NULL;
 	}
 
-	dbus_connection_unref(*conn);
+	dbus_connection_unref(conn);
 
-	(*upstart)->auto_start = FALSE;
+	upstart->auto_start = FALSE;
 
-	return;
+	return upstart;
 }
 
 gboolean
 upstart_app_launch_start_application (const gchar * appid, const gchar * const * uris)
 {
 	NihDBusProxy * proxy = NULL;
-	DBusConnection * conn = NULL;
 
-	nih_proxy_create(&proxy, &conn);
+	proxy = nih_proxy_create();
 	if (proxy == NULL) {
 		return FALSE;
 	}
@@ -379,9 +379,8 @@ gchar **
 upstart_app_launch_list_running_apps (void)
 {
 	NihDBusProxy * proxy = NULL;
-	DBusConnection * conn = NULL;
 
-	nih_proxy_create(&proxy, &conn);
+	proxy = nih_proxy_create();
 	if (proxy == NULL) {
 		return g_new0(gchar *, 1);
 	}
@@ -462,9 +461,8 @@ GPid
 upstart_app_launch_check_app_running (const gchar * appid)
 {
 	NihDBusProxy * proxy = NULL;
-	DBusConnection * conn = NULL;
 
-	nih_proxy_create(&proxy, &conn);
+	proxy = nih_proxy_create();
 	if (proxy == NULL) {
 		return 0;
 	}
