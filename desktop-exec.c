3abd53051a6b21e1ae4e0428dcfeb6a2912ab0e7
Ted Gould <ted@gould.cx>
2013-09-20 22:18:41 -0500

    Change it so that we do a lot of execing

--- desktop-exec.c
+++ desktop-exec.c
@@ -21,6 +21,7 @@
 #include <string.h>
 #include <glib.h>
 #include <gio/gio.h>
+#include <sys/apparmor.h>
 
 #include "helpers.h"
 
@@ -42,21 +43,36 @@ main (int argc, char * argv[])
 	gchar * execline = g_key_file_get_string(keyfile, "Desktop Entry", "Exec", NULL);
 	g_return_val_if_fail(execline != NULL, 1);
 
-	gchar * codeexec = desktop_exec_parse(execline, argc == 3 ? argv[2] : NULL);
-	if (codeexec != NULL) {
-		g_free(execline);
-		execline = codeexec;
+	gchar ** splitexec = g_strsplit(execline, " ", -1);
+	g_free(execline);
+
+	if (splitexec == NULL || splitexec[0] == NULL) {
+		g_key_file_free(keyfile);
+		g_free(splitexec);
+		return 1;
 	}
+
+	GArray * newargv = g_array_sized_new(TRUE, FALSE, 10, sizeof(gchar *));
+	int i;
+	for (i = 0; splitexec[i] != NULL; i++) {
+		gchar * execinserted = desktop_exec_parse(splitexec[i], argc == 3 ? argv[2] : NULL);
+		g_array_append_val(newargv, execinserted);
+	}
+	g_strfreev(splitexec);
 
 	gchar * apparmor = g_key_file_get_string(keyfile, "Desktop Entry", "XCanonicalAppArmorProfile", NULL);
 	if (apparmor != NULL) {
-		set_upstart_variable("APP_EXEC_POLICY", apparmor);
+		aa_change_onexec(apparmor);
+		g_free(apparmor);
 	}
 
-	set_upstart_variable("APP_EXEC", execline);
-
 	g_key_file_free(keyfile);
-	g_free(execline);
 
-	return 0;
+	gchar ** nargv = (gchar**)g_array_free(newargv, FALSE);
+
+	int execret = execvp(nargv[0], nargv);
+
+	g_strfreev(nargv);
+
+	return execret;
 }
