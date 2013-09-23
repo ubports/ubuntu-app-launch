34f01a540bc83ed0555b2b36185b397e5255b170
Ted Gould <ted@gould.cx>
2013-09-23 15:46:08 -0500

    Adding our first stub test

---
+++ tests/helper-test.cc
@@ -0,0 +1,17 @@
+
+#include <gtest/gtest.h>
+
+class HelperTest : public ::testing::Test
+{
+	private:
+
+	protected:
+		virtual void SetUp() {
+			return;
+		}
+};
+
+TEST_F(HelperTest, StubTest)
+{
+	return;
+}
