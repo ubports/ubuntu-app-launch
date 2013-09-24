3a13dd3a75b8d4c72ddb36c92accc1639e9a56ce
Ted Gould <ted@gould.cx>
2013-09-23 21:02:11 -0500

    More tests including non-supported and adding comments

--- tests/helper-test.cc
+++ tests/helper-test.cc
@@ -59,75 +59,109 @@ TEST_F(HelperTest, DesktopExecParse)
 {
 	GArray * output;
 
+	/* No %s and no URLs */
 	output = desktop_exec_parse("foo", NULL);
 	ASSERT_EQ(output->len, 1);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	g_array_free(output, TRUE);
 
+	/* URL without any % items */
 	output = desktop_exec_parse("foo", "http://ubuntu.com");
 	ASSERT_EQ(output->len, 1);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	g_array_free(output, TRUE);
 
+	/* Little u with a single URL */
 	output = desktop_exec_parse("foo %u", "http://ubuntu.com");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "http://ubuntu.com");
 	g_array_free(output, TRUE);
 
+	/* Little u with a NULL string */
+	output = desktop_exec_parse("foo %u", "");
+	ASSERT_EQ(output->len, 2);
+	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
+	ASSERT_STREQ(g_array_index(output, gchar *, 1), "");
+	g_array_free(output, TRUE);
+
+	/* Big %U with a single URL */
 	output = desktop_exec_parse("foo %U", "http://ubuntu.com");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "http://ubuntu.com");
 	g_array_free(output, TRUE);
 
+	/* Little %u by itself */
 	output = desktop_exec_parse("foo %u", "http://ubuntu.com http://slashdot.org");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "http://ubuntu.com");
 	g_array_free(output, TRUE);
 
+	/* Little %u in quotes */
 	output = desktop_exec_parse("foo \"%u\"", "http://ubuntu.com");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "http://ubuntu.com");
 	g_array_free(output, TRUE);
 
+	/* Single escaped " before the URL as a second param */
 	output = desktop_exec_parse("foo \\\"%u", "http://ubuntu.com");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "\"http://ubuntu.com");
 	g_array_free(output, TRUE);
 
+	/* URL is a quote, make sure we have it */
 	output = desktop_exec_parse("foo %u", "\"");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "\"");
 	g_array_free(output, TRUE);
 
+	/* Lots of quotes, escaped and not */
 	output = desktop_exec_parse("foo \\\"\"%u\"", "\"");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "\"\"");
 	g_array_free(output, TRUE);
 
+	/* Let's have no params, but a little %u */
 	output = desktop_exec_parse("foo\\ %u", "bar");
 	ASSERT_EQ(output->len, 1);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo bar");
 	g_array_free(output, TRUE);
 
+	/* Big U with two URLs */
 	output = desktop_exec_parse("foo %U", "http://ubuntu.com http://slashdot.org");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "http://ubuntu.com http://slashdot.org");
 	g_array_free(output, TRUE);
 
+	/* %f with a valid file */
 	output = desktop_exec_parse("foo %f", "file:///proc/version");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "/proc/version");
 	g_array_free(output, TRUE);
 
+	/* A %f with a NULL string */
+	output = desktop_exec_parse("foo %f", "");
+	ASSERT_EQ(output->len, 2);
+	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
+	ASSERT_STREQ(g_array_index(output, gchar *, 1), "");
+	g_array_free(output, TRUE);
+
+	/* %f with a URL that isn't a file */
+	output = desktop_exec_parse("foo %f", "torrent://moviephone.com/hot-new-movie");
+	ASSERT_EQ(output->len, 2);
+	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
+	ASSERT_STREQ(g_array_index(output, gchar *, 1), "");
+	g_array_free(output, TRUE);
+
+	/* Lots of %f combinations */
 	output = desktop_exec_parse("foo %f \"%f\" %f%f %f\\ %f", "file:///proc/version");
 	ASSERT_EQ(output->len, 5);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
@@ -137,24 +171,42 @@ TEST_F(HelperTest, DesktopExecParse)
 	ASSERT_STREQ(g_array_index(output, gchar *, 4), "/proc/version /proc/version");
 	g_array_free(output, TRUE);
 
+	/* Little f with two files */
 	output = desktop_exec_parse("foo %f", "file:///proc/version file:///proc/uptime");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "/proc/version");
 	g_array_free(output, TRUE);
 
+	/* Big F with two files */
 	output = desktop_exec_parse("foo %F", "file:///proc/version file:///proc/uptime");
 	ASSERT_EQ(output->len, 2);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "/proc/version /proc/uptime");
 	g_array_free(output, TRUE);
 
+	/* Groups of percents */
 	output = desktop_exec_parse("foo %% \"%%\" %%%%", NULL);
 	ASSERT_EQ(output->len, 4);
 	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
 	ASSERT_STREQ(g_array_index(output, gchar *, 1), "%");
 	ASSERT_STREQ(g_array_index(output, gchar *, 2), "%");
 	ASSERT_STREQ(g_array_index(output, gchar *, 3), "%%");
+	g_array_free(output, TRUE);
+
+	/* All the % sequences we don't support */
+	output = desktop_exec_parse("foo %d %D %n %N %v %m %i %c %k", "file:///proc/version");
+	ASSERT_EQ(output->len, 10);
+	ASSERT_STREQ(g_array_index(output, gchar *, 0), "foo");
+	ASSERT_STREQ(g_array_index(output, gchar *, 1), "");
+	ASSERT_STREQ(g_array_index(output, gchar *, 2), "");
+	ASSERT_STREQ(g_array_index(output, gchar *, 3), "");
+	ASSERT_STREQ(g_array_index(output, gchar *, 4), "");
+	ASSERT_STREQ(g_array_index(output, gchar *, 5), "");
+	ASSERT_STREQ(g_array_index(output, gchar *, 6), "");
+	ASSERT_STREQ(g_array_index(output, gchar *, 7), "");
+	ASSERT_STREQ(g_array_index(output, gchar *, 8), "");
+	ASSERT_STREQ(g_array_index(output, gchar *, 9), "");
 	g_array_free(output, TRUE);
 
 
