54243f9fc651c05c6a522e4c5751240e8ae95de7
Ted Gould <ted@gould.cx>
2013-09-27 11:53:07 -0500

    Set up callbacks for the focus and resume events

--- tests/second-exec-test.cc
+++ tests/second-exec-test.cc
@@ -21,15 +21,39 @@
 
 extern "C" {
 #include "../second-exec-core.h"
+#include "upstart-app-launch.h"
 #include "upstart-app-launch-mock.h"
 }
 
 class SecondExecTest : public ::testing::Test
 {
 	private:
+		gchar * last_focus_appid = NULL;
+		gchar * last_resume_appid = NULL;
+
+		static void focus_cb (const gchar * appid, gpointer user_data) {
+			SecondExecTest * _this = static_cast<SecondExecTest *>(user_data);
+			g_free(_this->last_focus_appid);
+			_this->last_focus_appid = g_strdup(appid);
+		}
+
+		static void resume_cb (const gchar * appid, gpointer user_data) {
+			SecondExecTest * _this = static_cast<SecondExecTest *>(user_data);
+			g_free(_this->last_resume_appid);
+			_this->last_resume_appid = g_strdup(appid);
+		}
 
 	protected:
 		virtual void SetUp() {
+			upstart_app_launch_observer_add_app_focus(focus_cb, this);
+			upstart_app_launch_observer_add_app_resume(resume_cb, this);
+
+			return;
+		}
+		virtual void TearDown() {
+			upstart_app_launch_observer_delete_app_focus(focus_cb, this);
+			upstart_app_launch_observer_delete_app_resume(resume_cb, this);
+
 			return;
 		}
 };
