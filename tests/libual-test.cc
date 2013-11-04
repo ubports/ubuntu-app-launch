2397d7a15a6f7c579a267cb9e98e68c72f4515b3
Ted Gould <ted@gould.cx>
2013-11-04 12:34:37 -0600

    Mock up starting an application

--- tests/libual-test.cc
+++ tests/libual-test.cc
@@ -22,6 +22,7 @@
 
 extern "C" {
 #include "upstart-app-launch.h"
+#include "libdbustest/dbus-test.h"
 }
 
 class LibUAL : public ::testing::Test
@@ -41,7 +42,27 @@ class LibUAL : public ::testing::Test
 
 };
 
-TEST_F(LibUAL, Dummy)
+TEST_F(LibUAL, StartApplication)
 {
+	DbusTestService * service = dbus_test_service_new(NULL);
+	DbusTestDbusMock * mock = dbus_test_dbus_mock_new("com.ubuntu.Upstart");
+	DbusTestDbusMockObject * obj = dbus_test_dbus_mock_get_object(mock, "/com/ubuntu/Upstart", "com.ubuntu.Upstart0_6", NULL);
+
+	dbus_test_dbus_mock_object_add_method(mock, obj,
+		"EmitEvent",
+		G_VARIANT_TYPE("(sasb)"),
+		NULL,
+		"",
+		NULL);
+
+	dbus_test_service_add_task(service, DBUS_TEST_TASK(mock));
+	dbus_test_service_start_tasks(service);
+
+	ASSERT_TRUE(upstart_app_launch_start_application("foo", NULL));
+	ASSERT_EQ(dbus_test_dbus_mock_object_check_method_call(mock, obj, "EmitEvent", NULL, NULL), 1);
+
+	g_object_unref(mock);
+	g_object_unref(service);
+
 	return;
 }
