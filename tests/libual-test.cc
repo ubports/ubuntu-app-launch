9f67c4543e2b570d61d52cd7869d52958dd4702c
Ted Gould <ted@gould.cx>
2013-11-04 13:16:31 -0600

    Fleshing out some of the start tests

--- tests/libual-test.cc
+++ tests/libual-test.cc
@@ -40,9 +40,32 @@ class LibUAL : public ::testing::Test
 			return;
 		}
 
+		bool check_env (GVariant * env_array, const gchar * var, const gchar * value) {
+			GVariantIter iter;
+			g_variant_iter_init(&iter, env_array);
+			gchar * envvar = NULL;
+			bool found = false;
+
+			while (g_variant_iter_loop(&iter, "s", &envvar)) {
+				if (g_str_has_prefix(envvar, var)) {
+					if (found) {
+						g_warning("Found the env var more than once!");
+						return false;
+					}
+
+					gchar * combined = g_strdup_printf("%s=%s", var, value);
+					if (g_strcmp0(envvar, combined) == 0) {
+						found = true;
+					}
+					g_free(combined);
+				}
+			}
+
+			return found;
+		}
 };
 
-TEST_F(LibUAL, BasicApplicationControl)
+TEST_F(LibUAL, StartApplication)
 {
 	DbusTestService * service = dbus_test_service_new(NULL);
 	DbusTestDbusMock * mock = dbus_test_dbus_mock_new("com.ubuntu.Upstart");
@@ -58,16 +81,63 @@ TEST_F(LibUAL, BasicApplicationControl)
 	dbus_test_service_add_task(service, DBUS_TEST_TASK(mock));
 	dbus_test_service_start_tasks(service);
 
+	/* Basic make sure we can send the event */
 	ASSERT_TRUE(upstart_app_launch_start_application("foo", NULL));
 	ASSERT_EQ(dbus_test_dbus_mock_object_check_method_call(mock, obj, "EmitEvent", NULL, NULL), 1);
 
-	ASSERT_TRUE(dbus_test_dbus_mock_object_clear_method_calls(mock, obj));
+	ASSERT_TRUE(dbus_test_dbus_mock_object_clear_method_calls(mock, obj, NULL));
 
-	ASSERT_TRUE(upstart_app_launch_stop_application("foo"));
-	ASSERT_EQ(dbus_test_dbus_mock_object_check_method_call(mock, obj, "EmitEvent", NULL, NULL), 1);
+	/* Now look at the details of the call */
+	ASSERT_TRUE(upstart_app_launch_start_application("foo", NULL));
 
+	guint len = 0;
+	const DbusTestDbusMockCall * calls = dbus_test_dbus_mock_object_get_method_calls(mock, obj, "EmitEvent", &len, NULL);
+	ASSERT_NE(calls, nullptr);
+	ASSERT_EQ(len, 1);
+
+	ASSERT_STREQ(calls->name, "EmitEvent");
+	ASSERT_EQ(g_variant_n_children(calls->params), 3);
+
+	GVariant * name = g_variant_get_child_value(calls->params, 0);
+	ASSERT_STREQ(g_variant_get_string(name, NULL), "application-start");
+	g_variant_unref(name);
+
+	GVariant * block = g_variant_get_child_value(calls->params, 2);
+	ASSERT_FALSE(g_variant_get_boolean(block));
+	g_variant_unref(block);
+
+	GVariant * env = g_variant_get_child_value(calls->params, 1);
+	ASSERT_TRUE(check_env(env, "APP_ID", "foo"));
+	g_variant_unref(env);
+
+	ASSERT_TRUE(dbus_test_dbus_mock_object_clear_method_calls(mock, obj, NULL));
+
+	/* Let's pass some URLs */
+	const gchar * urls[] = {
+		"http://ubuntu.com/",
+		"https://ubuntu.com/",
+		"file:///home/phablet/test.txt",
+		NULL
+	};
+	ASSERT_TRUE(upstart_app_launch_start_application("foo", urls));
+
+	len = 0;
+	calls = dbus_test_dbus_mock_object_get_method_calls(mock, obj, "EmitEvent", &len, NULL);
+	ASSERT_NE(calls, nullptr);
+	ASSERT_EQ(len, 1);
+
+	env = g_variant_get_child_value(calls->params, 1);
+	ASSERT_TRUE(check_env(env, "APP_ID", "foo"));
+	gchar * joined = g_strjoinv(" ", (gchar **)urls);
+	ASSERT_TRUE(check_env(env, "APP_URIS", joined));
+	g_free(joined);
+	g_variant_unref(env);
+
+
+	/* cleanup */
 	g_object_unref(mock);
 	g_object_unref(service);
 
 	return;
 }
+
