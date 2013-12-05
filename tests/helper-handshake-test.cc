e416e931b111a9671927a26e8facc093ba81de27
Ted Gould <ted@gould.cx>
2013-12-05 18:13:00 +0000

    Check to make sure we can handshake

--- tests/helper-handshake-test.cc
+++ tests/helper-handshake-test.cc
@@ -31,7 +31,10 @@ class HelperHandshakeTest : public ::testing::Test
 		GTestDBus * testbus = NULL;
 
 	protected:
+		GMainLoop * mainloop = NULL;
+
 		virtual void SetUp() {
+			mainloop = g_main_loop_new(NULL, FALSE);
 			testbus = g_test_dbus_new(G_TEST_DBUS_NONE);
 			g_test_dbus_up(testbus);
 		}
@@ -41,9 +44,55 @@ class HelperHandshakeTest : public ::testing::Test
 			g_object_unref(testbus);
 			return;
 		}
+
+	public:
+		GDBusMessage * FilterFunc (GDBusConnection * conn, GDBusMessage * message, gboolean incomming) {
+			if (g_strcmp0(g_dbus_message_get_member(message), "UnityStartingBroadcast") == 0) {
+				GVariant * body = g_dbus_message_get_body(message);
+				GVariant * correct_body = g_variant_new("(s)", "fooapp");
+				g_variant_ref_sink(correct_body);
+
+				[body, correct_body]() {
+					ASSERT_TRUE(g_variant_equal(body, correct_body));
+				}();
+
+				g_variant_unref(correct_body);
+				g_main_loop_quit(mainloop);
+			}
+
+			return message;
+		}
+
 };
 
-TEST_F(HelperHandshakeTest, dummy)
+static GDBusMessage *
+filter_func (GDBusConnection * conn, GDBusMessage * message, gboolean incomming, gpointer user_data) {
+	return static_cast<HelperHandshakeTest *>(user_data)->FilterFunc(conn, message, incomming);
+}
+
+TEST_F(HelperHandshakeTest, BaseHandshake)
 {
+	GDBusConnection * con = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+	guint filter = g_dbus_connection_add_filter(con, filter_func, this, NULL);
+
+	handshake_t * handshake = starting_handshake_start("fooapp");
+
+	g_main_loop_run(mainloop);
+
+	g_dbus_connection_remove_filter(con, filter);
+	g_main_loop_unref(mainloop);
+
+	g_dbus_connection_emit_signal(con,
+		g_dbus_connection_get_unique_name(con), /* destination */
+		"/", /* path */
+		"com.canonical.UpstartAppLaunch", /* interface */
+		"UnityStartingSignal", /* signal */
+		g_variant_new("(s)", "fooapp"), /* params, the same */
+		NULL);
+
+	starting_handshake_wait(handshake);
+
+	g_object_unref(con);
+
 	return;
 }
