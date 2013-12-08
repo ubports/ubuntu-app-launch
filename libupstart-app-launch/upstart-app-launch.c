b696ce70effc1a27c43929b63271c4d365b718bf
Ted Gould <ted@gould.cx>
2013-12-08 14:02:36 -0700

    Plug a mem leak and print a nice debug message

--- libupstart-app-launch/upstart-app-launch.c
+++ libupstart-app-launch/upstart-app-launch.c
@@ -48,8 +48,11 @@ app_uris_string (const gchar * const * uris)
 static void
 application_start_cb (GObject * obj, GAsyncResult * res, gpointer user_data)
 {
+	gchar * appid = (gchar *)user_data;
 	GError * error = NULL;
 	GVariant * result = NULL;
+
+	g_debug("Application Started: %s", appid);
 
 	result = g_dbus_connection_call_finish(G_DBUS_CONNECTION(obj), res, &error);
 
@@ -60,6 +63,8 @@ application_start_cb (GObject * obj, GAsyncResult * res, gpointer user_data)
 		g_warning("Unable to emit event to start application: %s", error->message);
 		g_error_free(error);
 	}
+
+	g_free(appid);
 }
 
 gboolean
