4c29fb9b78b8b117a02d19b6d4f42a8845eba547
Ted Gould <ted@gould.cx>
2013-12-13 13:34:59 -0600

    Add in base exec util test

---
+++ tests/exec-util-test.cc
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Ted Gould <ted.gould@canonical.com>
+ */
+
+#include <gtest/gtest.h>
+#include <libdbustest/dbus-test.h>
+#include <gio/gio.h>
+
+class ExecUtil : public ::testing::Test
+{
+	protected:
+		DbusTestService * service = NULL;
+		DbusTestDbusMock * mock = NULL;
+		GDBusConnection * bus = NULL;
+
+	protected:
+		virtual void SetUp() {
+			service = dbus_test_service_new(NULL);
+
+			mock = dbus_test_dbus_mock_new("com.ubuntu.Upstart");
+
+			dbus_test_service_add_task(service, DBUS_TEST_TASK(mock));
+			dbus_test_service_start_tasks(service);
+
+			bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+			g_dbus_connection_set_exit_on_close(bus, FALSE);
+			g_object_add_weak_pointer(G_OBJECT(bus), (gpointer *)&bus);
+		}
+
+		virtual void TearDown() {
+			g_clear_object(&mock);
+			g_clear_object(&service);
+
+			g_object_unref(bus);
+
+			unsigned int cleartry = 0;
+			while (bus != NULL && cleartry < 100) {
+				g_usleep(100000);
+				while (g_main_pending()) {
+					g_main_iteration(TRUE);
+				}
+				cleartry++;
+			}
+		}
+};
+
+TEST_F(ExecUtil, Dummy)
+{
+
+}
