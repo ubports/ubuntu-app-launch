a991f9473751673d1023b57f6d1b441d7ac22c5a
Ted Gould <ted@gould.cx>
2014-09-10 10:17:15 -0500

    Basic framework for query

--- tools/ubuntu-app-usage.c
+++ tools/ubuntu-app-usage.c
@@ -17,19 +17,47 @@
  *     Ted Gould <ted.gould@canonical.com>
  */
 
+#include <zeitgeist.h>
+
+GPtrArray *
+build_event_templates (void)
+{
+	return g_ptr_array_new_with_free_func(g_object_unref);
+}
+
+void
+find_events_cb (GObject * obj, GAsyncResult * res, gpointer user_data)
+{
+
+	return;
+}
+
 int
 main (int argc, char * argv[])
 {
+	GMainLoop * main_loop = g_main_loop_new(NULL, FALSE);
+	ZeitgeistLog * log = zeitgeist_log_get_default();
 
+	GPtrArray * templates = build_event_templates();
+	ZeitgeistTimeRange * time = zeitgeist_time_range_new_anytime();
 
+	zeitgeist_log_find_events(log,
+		time, /* time range */
+		templates,
+		ZEITGEIST_STORAGE_STATE_ANY, /* storage state */
+		10000, /* num events */
+		ZEITGEIST_RELEVANT_RESULT_TYPE_RECENT, /* result type */
+		NULL, /* cancelable */
+		find_events_cb,
+		main_loop);
 
+	g_ptr_array_unref(templates);
+	g_object_unref(time);
 
+	g_main_loop_run(main_loop);
+	g_main_loop_unref(main_loop);
 
-
-
-
-
-
+	g_object_unref(log);
 
 	return 0;
 }
