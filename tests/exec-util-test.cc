72039ac071d023baccae2a9eaed3d8f9a5d7f6bd
Ted Gould <ted@gould.cx>
2015-08-10 21:13:34 -0500

    Refactor test into a helper function

--- tests/exec-util-test.cc
+++ tests/exec-util-test.cc
@@ -17,6 +17,9 @@
  *     Ted Gould <ted.gould@canonical.com>
  */
 
+#include <map>
+#include <functional>
+
 #include <gtest/gtest.h>
 #include <libdbustest/dbus-test.h>
 #include <gio/gio.h>
@@ -88,117 +91,94 @@ class ExecUtil : public ::testing::Test
 				cleartry++;
 			}
 		}
+
+		inline void StartCheckEnv (const std::string& appid, std::map<std::string, std::function<void(const gchar *)>> enums) {
+			std::map<std::string, bool> env_found;
+			DbusTestDbusMockObject * obj = dbus_test_dbus_mock_get_object(mock, "/com/test/job", "com.ubuntu.Upstart0_6.Job", nullptr);
+
+			g_setenv("TEST_CLICK_DB", "click-db-dir", TRUE);
+			g_setenv("TEST_CLICK_USER", "test-user", TRUE);
+			g_setenv("UBUNTU_APP_LAUNCH_LINK_FARM", CMAKE_SOURCE_DIR "/link-farm", TRUE);
+
+			ASSERT_TRUE(ubuntu_app_launch_start_application(appid.c_str(), nullptr));
+
+			guint len = 0;
+			const DbusTestDbusMockCall * calls = dbus_test_dbus_mock_object_get_method_calls(mock, obj, "Start", &len, nullptr);
+
+			ASSERT_EQ(1, len);
+			ASSERT_NE(nullptr, calls);
+			ASSERT_STREQ("Start", calls[0].name);
+
+			GVariant * envarray = g_variant_get_child_value(calls[0].params, 0);
+			GVariantIter iter;
+			g_variant_iter_init(&iter, envarray);
+			gchar * envvar = NULL;
+
+			while (g_variant_iter_loop(&iter, "s", &envvar)) {
+				g_debug("Looking at variable: %s", envvar);
+				gchar * var = g_strdup(envvar);
+
+				gchar * equal = g_strstr_len(var, -1, "=");
+				ASSERT_NE(equal, nullptr);
+
+				equal[0] = '\0';
+				gchar * value = &(equal[1]);
+
+				/* Test the variable */
+				auto varfunc = enums[var];
+				EXPECT_NE(nullptr, varfunc);
+				if (varfunc)
+					varfunc(value);
+
+				/* Mark it as found */
+				env_found[var] = true;
+
+				g_free(var);
+			}
+
+			g_variant_unref(envarray);
+
+			for(auto enumval : enums) {
+				EXPECT_TRUE(env_found[enumval.first]);
+			}
+		}
 };
+
+static void
+nocheck (const gchar *)
+{
+}
 
 TEST_F(ExecUtil, ClickExec)
 {
-	DbusTestDbusMockObject * obj = dbus_test_dbus_mock_get_object(mock, "/com/test/job", "com.ubuntu.Upstart0_6.Job", NULL);
-
-	g_setenv("TEST_CLICK_DB", "click-db-dir", TRUE);
-	g_setenv("TEST_CLICK_USER", "test-user", TRUE);
-	g_setenv("UBUNTU_APP_LAUNCH_LINK_FARM", CMAKE_SOURCE_DIR "/link-farm", TRUE);
-
-	ASSERT_TRUE(ubuntu_app_launch_start_application("com.test.good_application_1.2.3", NULL));
-
-	guint len = 0;
-	const DbusTestDbusMockCall * calls = dbus_test_dbus_mock_object_get_method_calls(mock, obj, "Start", &len, NULL);
-
-	ASSERT_EQ(1, len);
-	ASSERT_NE(nullptr, calls);
-	ASSERT_STREQ("Start", calls[0].name);
-
-	unsigned int i;
-
-	bool got_app_isolation = false;
-	bool got_cache_home = false;
-	bool got_config_home = false;
-	bool got_data_home = false;
-	bool got_runtime_dir = false;
-	bool got_data_dirs = false;
-	bool got_temp_dir = false;
-	bool got_shader_dir = false;
-	bool got_app_dir = false;
-	bool got_app_exec = false;
-	bool got_app_id = false;
-	bool got_app_pid = false;
-	bool got_app_desktop_path = false;
-
 #define APP_DIR CMAKE_SOURCE_DIR "/click-root-dir/.click/users/test-user/com.test.good"
 
-	GVariant * envarray = g_variant_get_child_value(calls[0].params, 0);
-	GVariantIter iter;
-	g_variant_iter_init(&iter, envarray);
-	gchar * envvar = NULL;
-
-	while (g_variant_iter_loop(&iter, "s", &envvar)) {
-		gchar * var = g_strdup(envvar);
-
-		gchar * equal = g_strstr_len(var, -1, "=");
-		ASSERT_NE(equal, nullptr);
-
-		equal[0] = '\0';
-		gchar * value = &(equal[1]);
-
-		if (g_strcmp0(var, "UBUNTU_APPLICATION_ISOLATION") == 0) {
-			EXPECT_STREQ("1", value);
-			got_app_isolation = true;
-		} else if (g_strcmp0(var, "XDG_CACHE_HOME") == 0) {
-			got_cache_home = true;
-		} else if (g_strcmp0(var, "XDG_CONFIG_HOME") == 0) {
-			got_config_home = true;
-		} else if (g_strcmp0(var, "XDG_DATA_HOME") == 0) {
-			got_data_home = true;
-		} else if (g_strcmp0(var, "XDG_RUNTIME_DIR") == 0) {
-			got_runtime_dir = true;
-		} else if (g_strcmp0(var, "XDG_DATA_DIRS") == 0) {
-			EXPECT_TRUE(g_str_has_prefix(value, APP_DIR ":"));
-			got_data_dirs = true;
-		} else if (g_strcmp0(var, "TMPDIR") == 0) {
-			EXPECT_TRUE(g_str_has_suffix(value, "com.test.good"));
-			got_temp_dir = true;
-		} else if (g_strcmp0(var, "__GL_SHADER_DISK_CACHE_PATH") == 0) {
-			EXPECT_TRUE(g_str_has_suffix(value, "com.test.good"));
-			got_shader_dir = true;
-		} else if (g_strcmp0(var, "APP_DIR") == 0) {
-			EXPECT_STREQ(APP_DIR, value);
-			got_app_dir = true;
-		} else if (g_strcmp0(var, "APP_EXEC") == 0) {
-			EXPECT_STREQ("foo", value);
-			got_app_exec = true;
-		} else if (g_strcmp0(var, "APP_ID") == 0) {
-			EXPECT_STREQ("com.test.good_application_1.2.3", value);
-			got_app_id = true;
-		} else if (g_strcmp0(var, "APP_LAUNCHER_PID") == 0) {
-			EXPECT_EQ(getpid(), atoi(value));
-			got_app_pid = true;
-		} else if (g_strcmp0(var, "APP_DESKTOP_FILE_PATH") == 0) {
-			EXPECT_STREQ(APP_DIR "/application.desktop", value);
-			got_app_desktop_path = true;
-		} else {
-			g_warning("Unknown variable! %s", var);
-			EXPECT_TRUE(false);
-		}
-
-		g_free(var);
-	}
-
-	g_variant_unref(envarray);
+	StartCheckEnv("com.test.good_application_1.2.3", {
+		{"UBUNTU_APPLICATION_ISOLATION", [](const gchar * value) {
+			EXPECT_STREQ("1", value); }},
+		{"XDG_CACHE_HOME", nocheck},
+		{"XDG_CONFIG_HOME", nocheck},
+		{"XDG_DATA_HOME", nocheck},
+		{"XDG_RUNTIME_DIR", nocheck},
+		{"XDG_DATA_DIRS", [](const gchar * value) {
+			EXPECT_TRUE(g_str_has_prefix(value, APP_DIR ":")); }},
+		{"TMPDIR", [](const gchar * value) {
+			EXPECT_TRUE(g_str_has_suffix(value, "com.test.good")); }},
+		{"__GL_SHADER_DISK_CACHE_PATH", [](const gchar * value) {
+			EXPECT_TRUE(g_str_has_suffix(value, "com.test.good")); }},
+		{"APP_DIR", [](const gchar * value) {
+			EXPECT_STREQ(APP_DIR, value); }},
+		{"APP_EXEC", [](const gchar * value) {
+			EXPECT_STREQ("foo", value); }},
+		{"APP_ID", [](const gchar * value) {
+			EXPECT_STREQ("com.test.good_application_1.2.3", value); }},
+		{"APP_LAUNCHER_PID", [](const gchar * value) {
+			EXPECT_EQ(getpid(), atoi(value)); }},
+		{"APP_DESKTOP_FILE_PATH", [](const gchar * value) {
+			EXPECT_STREQ(APP_DIR "/application.desktop", value); }},
+	});
 
 #undef APP_DIR
-
-	EXPECT_TRUE(got_app_isolation);
-	EXPECT_TRUE(got_cache_home);
-	EXPECT_TRUE(got_config_home);
-	EXPECT_TRUE(got_data_home);
-	EXPECT_TRUE(got_runtime_dir);
-	EXPECT_TRUE(got_data_dirs);
-	EXPECT_TRUE(got_temp_dir);
-	EXPECT_TRUE(got_shader_dir);
-	EXPECT_TRUE(got_app_dir);
-	EXPECT_TRUE(got_app_exec);
-	EXPECT_TRUE(got_app_id);
-	EXPECT_TRUE(got_app_pid);
-	EXPECT_TRUE(got_app_desktop_path);
 }
 
 TEST_F(ExecUtil, DesktopExec)
