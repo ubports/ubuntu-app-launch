6d964b65751430385929f421080c7fb33e2baa75
Ted Gould <ted@gould.cx>
2015-09-15 08:47:44 -0500

    Easier to read an a cast that is needed

--- helpers-shared.c
+++ helpers-shared.c
@@ -191,7 +191,10 @@ cgroup_manager_connection (void)
 	g_main_context_pop_thread_default(context);
 
 	if (!use_session_bus && connection.con != NULL) {
-		g_object_set_data_full(G_OBJECT(connection.con), "cgmanager-context", context, g_main_context_unref);
+		g_object_set_data_full(G_OBJECT(connection.con),
+		                       "cgmanager-context",
+		                       context,
+		                       (GDestroyNotify)g_main_context_unref);
 	} else {
 		g_main_context_unref(context);
 	}
