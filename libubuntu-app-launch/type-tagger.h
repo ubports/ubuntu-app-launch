8ef40da2db69a300b34a10c29bec964afcc309a8
Ted Gould <ted@gould.cx>
2016-02-09 16:51:12 -0600

    Adding a format target

--- libubuntu-app-launch/type-tagger.h
+++ libubuntu-app-launch/type-tagger.h
@@ -1,25 +1,37 @@
 #pragma once
 
-namespace ubuntu {
-namespace app_launch {
+namespace ubuntu
+{
+namespace app_launch
+{
 
 template <typename Tag, typename T>
-class TypeTagger {
+class TypeTagger
+{
 public:
-	static TypeTagger<Tag, T> from_raw(const T& value) {
-		return TypeTagger<Tag, T>(value);
-	}
-	const T& value() const {
-		return _value;
-	}
-	operator T() const {
-		return _value;
-	}
-	~TypeTagger (void) { }
+    static TypeTagger<Tag, T> from_raw(const T& value)
+    {
+        return TypeTagger<Tag, T>(value);
+    }
+    const T& value() const
+    {
+        return _value;
+    }
+    operator T() const
+    {
+        return _value;
+    }
+    ~TypeTagger(void)
+    {
+    }
+
 private:
-	TypeTagger(const T& value) : _value(value) { }
-	T _value;
+    TypeTagger(const T& value)
+        : _value(value)
+    {
+    }
+    T _value;
 };
 
-}; // namespace app_launch
-}; // namespace ubuntu
+};  // namespace app_launch
+};  // namespace ubuntu
