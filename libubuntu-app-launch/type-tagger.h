5fc4fab348f3c52b50526a3caf9e50081c48aa8e
Ted Gould <ted@gould.cx>
2016-04-18 13:13:42 -0500

    Run through the autoformatter

--- libubuntu-app-launch/type-tagger.h
+++ libubuntu-app-launch/type-tagger.h
@@ -7,27 +7,27 @@ namespace app_launch
 
 /** \brief A small template to make it clearer when special types are being used
 
-	The TypeTagger a small piece of C++ so that we can have custom types
-	for things in the Ubuntu App Launch API that should be handled in
-	special ways, but really have basic types at their core. In this way
-	there is explicit code to convert these items into their fundamental type
-	so that is obvious and can be easily searched for.
+        The TypeTagger a small piece of C++ so that we can have custom types
+        for things in the Ubuntu App Launch API that should be handled in
+        special ways, but really have basic types at their core. In this way
+        there is explicit code to convert these items into their fundamental type
+        so that is obvious and can be easily searched for.
 */
 template <typename Tag, typename T>
 class TypeTagger
 {
 public:
-	/** Function to build a TypeTagger object from a fundamental type */
+    /** Function to build a TypeTagger object from a fundamental type */
     static TypeTagger<Tag, T> from_raw(const T& value)
     {
         return TypeTagger<Tag, T>(value);
     }
-	/** Getter to get the fundamental type out of the TypeTagger wrapper */
+    /** Getter to get the fundamental type out of the TypeTagger wrapper */
     const T& value() const
     {
         return _value;
     }
-	/** Getter to get the fundamental type out of the TypeTagger wrapper */
+    /** Getter to get the fundamental type out of the TypeTagger wrapper */
     operator T() const
     {
         return _value;
@@ -37,7 +37,7 @@ public:
     }
 
 private:
-	/** Private constructor used by from_raw() */
+    /** Private constructor used by from_raw() */
     TypeTagger(const T& value)
         : _value(value)
     {
