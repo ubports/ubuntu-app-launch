d8b065640130daf21489ee3c113f5da8ce0075c8
Larry Price <larry.price@canonical.com>
2016-04-28 10:57:19 -0400

    adding test data to source control

---
+++ tests/application-icon-finder.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Larry Price <larry.price@canonical.com>
+ */
+
+#include "application-icon-finder.h"
+#include <gtest/gtest.h>
+#include <glib.h>
+
+TEST(ApplicationIconFinder, ReturnsDefaultPathWhenNoThemeFileAvailable)
+{
+    auto finder = ubuntu::app_launch::IconFinder::fromBasePath("/tmp/please/dont/put/stuff/here");
+    EXPECT_EQ("/tmp/please/dont/put/stuff/here/app", finder->find("app"));
+}
+
+TEST(ApplicationIconFinder, ReturnsDefaultPathWhenNoAppIconFound)
+{
+    auto dir = g_get_current_dir();
+    auto basePath = std::string(dir) + "/data";
+    g_free(dir);
+    auto finder = ubuntu::app_launch::IconFinder::fromBasePath(basePath);
+    EXPECT_EQ(basePath + "/app_unknown", finder->find("app_unknown"));
+}
+
+TEST(ApplicationIconFinder, ReturnsLargestAvailableIcon)
+{
+    auto dir = g_get_current_dir();
+    auto basePath = std::string(dir) + "/data";
+    g_free(dir);
+    auto finder = ubuntu::app_launch::IconFinder::fromBasePath(basePath);
+    EXPECT_EQ(basePath + "/usr/share/icons/hicolor/24x24/apps/app.xpm", finder->find("app"));
+}
+
+TEST(ApplicationIconFinder, ReturnsIconAsDirectlyGiven)
+{
+    auto dir = g_get_current_dir();
+    auto basePath = std::string(dir) + "/data";
+    g_free(dir);
+    auto finder = ubuntu::app_launch::IconFinder::fromBasePath(basePath);
+    EXPECT_EQ(basePath + "/usr/share/icons/hicolor/scalable/apps/app.svg", finder->find(basePath + "/usr/share/icons/hicolor/scalable/apps/app.svg"));
+}
+
+TEST(ApplicationIconFinder, ReturnsIconFromPixmapWhenGivenExtension)
+{
+    auto dir = g_get_current_dir();
+    auto basePath = std::string(dir) + "/data";
+    g_free(dir);
+    auto finder = ubuntu::app_launch::IconFinder::fromBasePath(basePath);
+    EXPECT_EQ(basePath + "/usr/share/pixmaps/app.png", finder->find("app.png"));
+}
