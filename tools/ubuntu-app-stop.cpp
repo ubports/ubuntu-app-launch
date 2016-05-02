6181c7f5f7f76ceafa9a6f0fa7f4fd927f73f7e9
Ted Gould <ted@gould.cx>
2016-05-02 17:01:00 -0500

    Make a more tollerant stop utility

--- tools/ubuntu-app-stop.cpp
+++ tools/ubuntu-app-stop.cpp
@@ -29,12 +29,21 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    auto appid = ubuntu::app_launch::AppID::parse(argv[1]);
-    auto app = ubuntu::app_launch::Application::create(appid, ubuntu::app_launch::Registry::getDefault());
+    auto appid = ubuntu::app_launch::AppID::find(argv[1]);
+    if (appid.empty()) {
+        std::cerr << "Unable to find app for appid: " << argv[1] << std::endl;
+        return 1;
+    }
 
-    for (auto instance : app->instances())
-    {
-        instance->stop();
+    try {
+        auto app = ubuntu::app_launch::Application::create(appid, ubuntu::app_launch::Registry::getDefault());
+
+        for (auto instance : app->instances())
+        {
+            instance->stop();
+        }
+    } catch (...) {
+        return 1;
     }
 
     return 0;
