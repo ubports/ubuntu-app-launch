c25989573158996c05608241b5955551fe265e40
Ted Gould <ted@gould.cx>
2016-06-09 16:01:20 -0500

    A bunch of stubs for a Snap backend

---
+++ libubuntu-app-launch/application-impl-snap.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Ted Gould <ted.gould@canonical.com>
+ */
+
+#include "application-impl-snap.h"
+#include "application-info-desktop.h"
+#include "registry-impl.h"
+
+namespace ubuntu
+{
+namespace app_launch
+{
+namespace app_impls
+{
+
+Snap::Snap(const AppID& appid, const std::shared_ptr<Registry>& registry)
+    : Base(registry)
+{
+}
+
+std::list<std::shared_ptr<Application>> Snap::list(const std::shared_ptr<Registry>& registry)
+{
+    return {};
+}
+
+AppID Snap::appId()
+{
+    return _appid;
+}
+
+std::shared_ptr<Application::Info> Snap::info()
+{
+    return {};
+}
+
+std::vector<std::shared_ptr<Application::Instance>> Snap::instances()
+{
+    return {};
+}
+
+std::shared_ptr<Application::Instance> Snap::launch(const std::vector<Application::URL>& urls)
+{
+    return {};
+}
+
+std::shared_ptr<Application::Instance> Snap::launchTest(const std::vector<Application::URL>& urls)
+{
+    return {};
+}
+
+}  // namespace app_impls
+}  // namespace app_launch
+}  // namespace ubuntu
---
+++ libubuntu-app-launch/application-impl-snap.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Ted Gould <ted.gould@canonical.com>
+ */
+
+#include "application-impl-base.h"
+#include <gio/gdesktopappinfo.h>
+#include <json-glib/json-glib.h>
+
+#pragma once
+
+namespace ubuntu
+{
+namespace app_launch
+{
+namespace app_impls
+{
+
+class Snap : public Base
+{
+public:
+    Snap(const AppID& appid, const std::shared_ptr<Registry>& registry);
+
+    static std::list<std::shared_ptr<Application>> list(const std::shared_ptr<Registry>& registry);
+
+    AppID appId() override;
+
+    std::shared_ptr<Info> info() override;
+
+    std::vector<std::shared_ptr<Instance>> instances() override;
+
+    std::shared_ptr<Instance> launch(const std::vector<Application::URL>& urls = {}) override;
+    std::shared_ptr<Instance> launchTest(const std::vector<Application::URL>& urls = {}) override;
+
+private:
+    AppID _appid;
+
+    std::shared_ptr<JsonObject> _manifest;
+    std::shared_ptr<GKeyFile> _keyfile;
+};
+
+}  // namespace app_impls
+}  // namespace app_launch
+}  // namespace ubuntu
