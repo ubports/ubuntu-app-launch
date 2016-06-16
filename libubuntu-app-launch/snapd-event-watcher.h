3eb0dc0cf6fb201fd2cbb45e3b25b2614b678f1d
Ted Gould <ted@gould.cx>
2016-06-15 22:03:44 -0500

    Adding a base snap event object

---
+++ libubuntu-app-launch/snapd-event-watcher.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Ted Gould <ted.gould@canonical.com>
+ */
+
+#pragma once
+
+namespace ubuntu
+{
+namespace app_launch
+{
+namespace app_impls
+{
+namespace snapd
+{
+
+class EventWatcher
+{
+public:
+    EventWatcher();
+    virtual ~EventWatcher() = default;
+};
+
+}  // namespace snapd
+}  // namespace app_impls
+}  // namespace app_launch
+}  // namespace ubuntu
