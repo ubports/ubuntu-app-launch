061cae05a2597dde72bb0cce78d060938fbe2c91
Ted Gould <ted@gould.cx>
2016-06-27 15:48:43 -0500

    Dropping click-exec.[ch]

--- libubuntu-app-launch/ubuntu-app-launch.cpp
+++ libubuntu-app-launch/ubuntu-app-launch.cpp
@@ -30,7 +30,6 @@ extern "C" {
 #include "ubuntu-app-launch-trace.h"
 #include "helpers.h"
 #include "ual-tracepoint.h"
-#include "click-exec.h"
 #include "desktop-exec.h"
 #include "recoverable-problem.h"
 #include "proxy-socket-demangler.h"
