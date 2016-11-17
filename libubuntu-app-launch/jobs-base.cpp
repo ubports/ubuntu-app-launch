a1d2f15d3d3132a694dda7ea842c3eb9357f3826
Ted Gould <ted@gould.cx>
2016-11-17 14:48:35 -0600

    Use the correct object type

--- libubuntu-app-launch/jobs-base.cpp
+++ libubuntu-app-launch/jobs-base.cpp
@@ -136,8 +136,8 @@ core::Signal<std::shared_ptr<Application>, std::shared_ptr<Application::Instance
                     }
 
                     auto sparams = std::shared_ptr<GVariant>(g_variant_ref(params), g_variant_unref);
-                    auto upstart = std::dynamic_pointer_cast<Upstart>(reg->impl->jobs);
-                    upstart->pauseEventEmitted(upstart->sig_appPaused, sparams, reg);
+                    auto manager = std::dynamic_pointer_cast<Base>(reg->impl->jobs);
+                    upstart->pauseEventEmitted(manager->sig_appPaused, sparams, reg);
                 },    /* callback */
                 data, /* user data */
                 [](gpointer user_data) {
@@ -183,8 +183,8 @@ core::Signal<std::shared_ptr<Application>, std::shared_ptr<Application::Instance
                     }
 
                     auto sparams = std::shared_ptr<GVariant>(g_variant_ref(params), g_variant_unref);
-                    auto upstart = std::dynamic_pointer_cast<Upstart>(reg->impl->jobs);
-                    upstart->pauseEventEmitted(upstart->sig_appResumed, sparams, reg);
+                    auto manager = std::dynamic_pointer_cast<Base>(reg->impl->jobs);
+                    upstart->pauseEventEmitted(manager->sig_appResumed, sparams, reg);
                 },    /* callback */
                 data, /* user data */
                 [](gpointer user_data) {
