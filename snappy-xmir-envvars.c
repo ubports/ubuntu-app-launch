a82e6b4c5f21c864ad643baf047ed7424809d1c3
Ted Gould <ted@gould.cx>
2016-11-21 11:28:14 -0600

    Starting utilities to put together an environment setup

---
+++ snappy-xmir-envvars.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Ted Gould <ted.gould@canonical.com>
+ */
+
+#include <stdlib.h>
+
+int
+main (int argc, char * argv[])
+{
+	/* Grab socket */
+
+
+	/* Dump envvars to socket */
+
+
+	/* Close the socket */
+
+
+	/* Wait for sigterm */
+
+
+	return EXIT_SUCCESS;
+}
---
+++ snappy-xmir.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Ted Gould <ted.gould@canonical.com>
+ */
+
+#include <stdlib.h>
+
+int
+main (int argc, char * argv[])
+{
+	/* Setup socket pair */
+
+
+	/* Fork and exec the x11 setup under it's confiment */
+
+
+	/* Read our socket until we get all of the environment */
+
+
+	/* Exec the application with the new environment under its confinement */
+
+
+	return EXIT_SUCCESS;
+}
