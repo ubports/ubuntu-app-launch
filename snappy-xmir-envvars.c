03d2e75e5d14ae71f1c7c2c976cce5361910e40f
Ted Gould <ted@gould.cx>
2016-11-21 14:42:46 -0600

    Fleshing out our utilities

--- snappy-xmir-envvars.c
+++ snappy-xmir-envvars.c
@@ -17,22 +17,62 @@
  *     Ted Gould <ted.gould@canonical.com>
  */
 
+#define _POSIX_C_SOURCE 200212L
+
+#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+
+void
+copyenv (int fd, const char * envname)
+{
+	const char * envval = getenv(envname);
+	if (envval == NULL) {
+		fprintf(stderr, "Unable to get environment variable '%s'\n", envname);
+		exit(EXIT_FAILURE);
+	}
+
+	write(fd, envname, strlen(envname) + 1);
+	write(fd, envval, strlen(envval) + 1);
+}
+
+void
+termhandler (int sig)
+{
+	exit(EXIT_SUCCESS);
+}
 
 int
 main (int argc, char * argv[])
 {
 	/* Grab socket */
+	char * socketstring = getenv("UBUNTU_APP_LAUNCH_SNAPPY_XMIR_ENVVARS");
+	if (socketstring == NULL) {
+		fprintf(stderr, "Unable to get socket environment variable\n");
+		return(EXIT_FAILURE);
+	}
 
+	int socketnum = atoi(socketstring);
+	if (!(socketnum > 0 && socketnum < 20)) {
+		fprintf(stderr, "Passed socket ID not within a valid range: %d\n", socketnum);
+		return(EXIT_FAILURE);
+	}
 
 	/* Dump envvars to socket */
-
+	copyenv(socketnum, "DISPLAY");
+	copyenv(socketnum, "DBUS_SESSION_BUS_ADDRESS");
 
 	/* Close the socket */
-
+	close(socketnum);
 
 	/* Wait for sigterm */
+	signal(SIGTERM, termhandler);
 
-
+	wait(NULL);
 	return EXIT_SUCCESS;
 }
--- snappy-xmir.c
+++ snappy-xmir.c
@@ -17,22 +17,98 @@
  *     Ted Gould <ted.gould@canonical.com>
  */
 
+#define _POSIX_C_SOURCE 200212L
+
+#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <string.h>
+
+void
+sigchild_handler (int signal)
+{
+	fprintf(stderr, "XMir has closed unexpectedly\n");
+	exit(EXIT_FAILURE);
+}
+
+struct sigaction sigchild_action = {
+	.sa_handler = sigchild_handler,
+	.sa_flags = SA_NOCLDWAIT
+};
 
 int
 main (int argc, char * argv[])
 {
-	/* Setup socket pair */
+	if (argc < 3) {
+		fprintf(stderr, "%s: Usage: [appid] [command to execute...]\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
+	char * appid = argv[1];
+
+	/* Setup socket pair */
+	int sockets[2];
+	if (socketpair(AF_LOCAL, SOCK_STREAM, 0, sockets) != 0) {
+		fprintf(stderr, "Unable to create socketpair for grabbing environment variables\n");
+		return EXIT_FAILURE;
+	}
+
+	int ourend = sockets[0];
+	int envend = sockets[1];
 
 	/* Fork and exec the x11 setup under it's confiment */
+	if (sigaction(SIGCHLD, &sigchild_action, NULL) != 0) {
+		fprintf(stderr, "Unable to setup child signal handler\n");
+		return EXIT_FAILURE;
+	}
 
+	if (fork() == 0) {
+		/* XMir start here */
+		/* GOAL: /snap/bin/unity8-session.xmir-helper $appid libertine-launch /snap/unity8-session/current/usr/bin/snappy-xmir-envvar */
+		/* TODO: Make paths more dynamic */
+		char socketbuf[16] = {0};
+		snprintf(socketbuf, 16, "%d", envend);
+
+		setenv("UBUNTU_APP_LAUNCH_SNAPPY_XMIR_ENVVARS", socketbuf, 1);
+
+		char * xmirexec[5] = {
+			"/snap/bin/unity8-session.xmir-helper",
+			appid,
+			"libertine-launch",
+			"/snap/unity8-session/current/usr/bin/snappy-xmir-envvar",
+			NULL
+		};
+
+		printf("Executing xmir-helper on PID: %d", getpid());
+
+		return execv(xmirexec[0], xmirexec);
+	}
 
 	/* Read our socket until we get all of the environment */
+	char readbuf[2048] = {0};
+	int amountread = 0;
+	while ((amountread = read(ourend, readbuf, 2048))) {
+		if (amountread == 2048) {
+			fprintf(stderr, "Environment is too large, abort!\n");
+			exit(EXIT_FAILURE);
+		}
 
+		char * startvar = readbuf;
+
+		/* TODO: Assumes one big read of both value and variable */
+		do {
+			char * startval = startvar + strlen(startvar) + 1;
+			setenv(startvar, startval, 1);
+			startvar = startval + strlen(startval) + 1;
+		}
+		while (startvar < readbuf + amountread);
+	}
+
+	close(ourend);
 
 	/* Exec the application with the new environment under its confinement */
-
-
-	return EXIT_SUCCESS;
+	return execv(argv[2], &(argv[2]));
 }
