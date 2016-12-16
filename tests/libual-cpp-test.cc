b67f79abb2eacc92fec2e2ecaf0868ea578f4194
Ted Gould <ted@gould.cx>
2016-12-16 11:23:55 -0600

    Put the spew master into its own header

--- tests/libual-cpp-test.cc
+++ tests/libual-cpp-test.cc
@@ -37,6 +37,7 @@
 
 #include "eventually-fixture.h"
 #include "mir-mock.h"
+#include "spew-master.h"
 
 #ifdef ENABLE_SNAPPY
 #include "snapd-mock.h"
@@ -1452,127 +1453,6 @@ TEST_F(LibUAL, StartStopHelperObserver)
         ubuntu_app_launch_observer_delete_helper_started(helper_observer_cb, "my-type-is-scorpio", &start_data));
     ASSERT_TRUE(ubuntu_app_launch_observer_delete_helper_stop(helper_observer_cb, "my-type-is-libra", &stop_data));
 }
-
-class SpewMaster
-{
-public:
-    SpewMaster()
-        : thread(
-              [this]() {
-                  gint spewstdout = 0;
-                  std::array<const gchar*, 2> spewline{SPEW_UTILITY, nullptr};
-                  ASSERT_TRUE(g_spawn_async_with_pipes(NULL,                    /* directory */
-                                                       (char**)spewline.data(), /* command line */
-                                                       NULL,                    /* environment */
-                                                       G_SPAWN_DEFAULT,         /* flags */
-                                                       NULL,                    /* child setup */
-                                                       NULL,                    /* child setup */
-                                                       &pid_,                   /* pid */
-                                                       NULL,                    /* stdin */
-                                                       &spewstdout,             /* stdout */
-                                                       NULL,                    /* stderr */
-                                                       NULL));                  /* error */
-
-                  spewoutchan = g_io_channel_unix_new(spewstdout);
-                  g_io_channel_set_flags(spewoutchan, G_IO_FLAG_NONBLOCK, NULL);
-
-                  iosource = g_io_create_watch(spewoutchan, G_IO_IN);
-                  g_source_set_callback(iosource, (GSourceFunc)datain, this, nullptr);
-                  g_source_attach(iosource, g_main_context_get_thread_default());
-
-                  /* Setup our OOM adjust file */
-                  gchar* procdir = g_strdup_printf(CMAKE_BINARY_DIR "/libual-proc/%d", pid_);
-                  ASSERT_EQ(0, g_mkdir_with_parents(procdir, 0700));
-                  oomadjfile = g_strdup_printf("%s/oom_score_adj", procdir);
-                  g_free(procdir);
-                  ASSERT_TRUE(g_file_set_contents(oomadjfile, "0", -1, NULL));
-              },
-              [this]() {
-                  /* Clean up */
-                  gchar* killstr = g_strdup_printf("kill -9 %d", pid_);
-                  ASSERT_TRUE(g_spawn_command_line_sync(killstr, NULL, NULL, NULL, NULL));
-                  g_free(killstr);
-
-                  g_source_destroy(iosource);
-                  g_io_channel_unref(spewoutchan);
-                  g_clear_pointer(&oomadjfile, g_free);
-              })
-    {
-        datacnt_ = 0;
-    }
-
-    ~SpewMaster()
-    {
-    }
-
-    std::string oomScore()
-    {
-        gchar* oomvalue = nullptr;
-        g_file_get_contents(oomadjfile, &oomvalue, nullptr, nullptr);
-        if (oomvalue != nullptr)
-        {
-            return std::string(oomvalue);
-        }
-        else
-        {
-            return {};
-        }
-    }
-
-    GPid pid()
-    {
-        return pid_;
-    }
-
-    gsize dataCnt()
-    {
-        g_debug("Data Count for %d: %d", pid_, int(datacnt_));
-        return datacnt_;
-    }
-
-    void reset()
-    {
-        bool endofqueue = thread.executeOnThread<bool>([this]() {
-            while (G_IO_STATUS_AGAIN == g_io_channel_flush(spewoutchan, nullptr))
-                ;
-            return true; /* the main loop has processed */
-        });
-        g_debug("Reset %d", pid_);
-        if (endofqueue)
-            datacnt_ = 0;
-        else
-            g_warning("Unable to clear mainloop on reset");
-    }
-
-private:
-    std::atomic<gsize> datacnt_;
-    GPid pid_ = 0;
-    gchar* oomadjfile = nullptr;
-    GIOChannel* spewoutchan = nullptr;
-    GSource* iosource = nullptr;
-    GLib::ContextThread thread;
-
-    static gboolean datain(GIOChannel* source, GIOCondition cond, gpointer data)
-    {
-        auto spew = static_cast<SpewMaster*>(data);
-        gchar* str = NULL;
-        gsize len = 0;
-        GError* error = NULL;
-
-        g_io_channel_read_line(source, &str, &len, NULL, &error);
-        g_free(str);
-
-        if (error != NULL)
-        {
-            g_warning("Unable to read from channel: %s", error->message);
-            g_error_free(error);
-        }
-
-        spew->datacnt_ += len;
-
-        return TRUE;
-    }
-};
 
 // DISABLED: Skipping these tests to not block on bug #1584849
 TEST_F(LibUAL, DISABLED_PauseResume)
