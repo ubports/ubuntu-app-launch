83a9f601a36365d7f0f2491f30311214558a745f
Ted Gould <ted@gould.cx>
2017-01-13 13:59:43 -0600

    Pull out the registry-mock into it's own file for sharing

---
+++ tests/registry-mock.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2017 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Ted Gould <ted.gould@canonical.com>
+ */
+
+#pragma once
+
+#include "registry-impl.h"
+#include "registry.h"
+
+#include <gmock/gmock.h>
+
+class RegistryImplMock : public ubuntu::app_launch::Registry::Impl
+{
+public:
+    RegistryImplMock(ubuntu::app_launch::Registry* reg)
+        : ubuntu::app_launch::Registry::Impl(reg)
+    {
+    }
+
+    MOCK_METHOD2(zgSendEvent, void(ubuntu::app_launch::AppID, const std::string& eventtype));
+};
+
+class RegistryMock : public ubuntu::app_launch::Registry
+{
+public:
+    RegistryMock()
+    {
+        impl = std::unique_ptr<RegistryImplMock>(new RegistryImplMock(this));
+    }
+};
