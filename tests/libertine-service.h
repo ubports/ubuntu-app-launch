e43f2f5395fafc9edad9ce0e570a4ccbb775efca
Ted Gould <ted@gould.cx>
2017-02-13 15:17:25 -0600

    Creating a helper class for the service

---
+++ tests/libertine-service.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2017 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *      Ted Gould <ted@canonical.com>
+ */
+
+#include <gio/gio.h>
+
+#include <libdbustest/dbus-test.h>
+
+class LibertineService
+{
+private:
+    DbusTestProcess* process = nullptr;
+
+public:
+    SystemdMock()
+    {
+        process = dbus_test_process_new("/usr/bin/libertined");
+        dbus_test_process_append_param(process, "--debug");
+
+        dbus_test_task_set_bus(DBUS_TEST_TASK(process), DBUS_TEST_SERVICE_BUS_SESSION);
+        dbus_test_task_set_name(DBUS_TEST_TASK(process), "libertine");
+        dbus_test_task_set_return(DBUS_TEST_TASK(process), DBUS_TEST_TASK_RETURN_IGNORE);
+        dbus_test_task_wait_finished(DBUS_TEST_TASK(process), FALSE);
+    }
+
+    ~SystemdMock()
+    {
+        g_debug("Destroying the Libertined Task");
+        g_clear_object(&process);
+    }
+
+    operator std::shared_ptr<DbusTestTask>()
+    {
+        std::shared_ptr<DbusTestTask> retval(DBUS_TEST_TASK(g_object_ref(process)),
+                                             [](DbusTestTask* process) { g_clear_object(&process); });
+        return retval;
+    }
+
+    operator DbusTestTask*()
+    {
+        return DBUS_TEST_TASK(process);
+    }
+};
