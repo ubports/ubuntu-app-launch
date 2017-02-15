80d942344f3db264ee7e4f7a61b204fe1ee161f9
Ted Gould <ted@gould.cx>
2017-02-15 15:01:27 -0600

    Remove the click name from the helper implementation class

--- libubuntu-app-launch/helper.cpp
+++ libubuntu-app-launch/helper.cpp
@@ -31,48 +31,7 @@ namespace app_launch
 namespace helper_impls
 {
 
-class Click : public Helper
-{
-public:
-    Click(const Helper::Type& type, const AppID& appid, const std::shared_ptr<Registry>& registry)
-        : _type(type)
-        , _appid(appid)
-        , _registry(registry)
-    {
-    }
-
-    AppID appId() override
-    {
-        return _appid;
-    }
-
-    bool hasInstances() override;
-    std::vector<std::shared_ptr<Helper::Instance>> instances() override;
-
-    std::shared_ptr<Helper::Instance> launch(std::vector<Helper::URL> urls = {}) override;
-    std::shared_ptr<Helper::Instance> launch(MirPromptSession* session, std::vector<Helper::URL> urls = {}) override;
-
-    static std::list<std::shared_ptr<Helper>> running(Helper::Type type, std::shared_ptr<Registry> registry);
-
-private:
-    Helper::Type _type;
-    AppID _appid;
-    std::shared_ptr<Registry> _registry;
-};
-
-bool Click::hasInstances()
-{
-    return _registry->impl->thread.executeOnThread<bool>([this]() {
-        auto instances = ubuntu_app_launch_list_helper_instances(_type.value().c_str(), ((std::string)_appid).c_str());
-        auto retval = (g_strv_length(instances) != 0);
-
-        g_strfreev(instances);
-
-        return retval;
-    });
-}
-
-class ClickInstance : public Helper::Instance
+class BaseInstance : public Helper::Instance
 {
 public: /* all one file, no one to hide from */
     AppID _appid;
@@ -80,10 +39,10 @@ public: /* all one file, no one to hide from */
     std::string _instanceid;
     std::shared_ptr<Registry> _registry;
 
-    ClickInstance(const AppID& appid,
-                  const Helper::Type& type,
-                  const std::string& instanceid,
-                  std::shared_ptr<Registry> registry)
+    BaseInstance(const AppID& appid,
+                 const Helper::Type& type,
+                 const std::string& instanceid,
+                 std::shared_ptr<Registry> registry)
         : _appid(appid)
         , _type(type)
         , _instanceid(instanceid)
@@ -122,23 +81,55 @@ public: /* all one file, no one to hide from */
     }
 };
 
-std::vector<std::shared_ptr<Click::Instance>> Click::instances()
+class Base : public Helper
 {
-    return _registry->impl->thread.executeOnThread<std::vector<std::shared_ptr<Click::Instance>>>(
-        [this]() -> std::vector<std::shared_ptr<Click::Instance>> {
-            std::vector<std::shared_ptr<Click::Instance>> vect;
-            auto instances =
-                ubuntu_app_launch_list_helper_instances(_type.value().c_str(), ((std::string)_appid).c_str());
-            for (int i = 0; instances[i] != nullptr; i++)
-            {
-                auto inst = std::make_shared<ClickInstance>(_appid, _type, instances[i], _registry);
-                vect.push_back(inst);
-            }
+public:
+    Base(const Helper::Type& type, const AppID& appid, const std::shared_ptr<Registry>& registry)
+        : _type(type)
+        , _appid(appid)
+        , _registry(registry)
+    {
+    }
 
-            g_strfreev(instances);
+    AppID appId() override
+    {
+        return _appid;
+    }
 
-            return vect;
-        });
+    bool hasInstances() override;
+    std::vector<std::shared_ptr<Helper::Instance>> instances() override;
+
+    std::shared_ptr<Helper::Instance> launch(std::vector<Helper::URL> urls = {}) override;
+    std::shared_ptr<Helper::Instance> launch(MirPromptSession* session, std::vector<Helper::URL> urls = {}) override;
+
+    static std::list<std::shared_ptr<Helper>> running(Helper::Type type, std::shared_ptr<Registry> registry);
+
+private:
+    Helper::Type _type;
+    AppID _appid;
+    std::shared_ptr<Registry> _registry;
+};
+
+bool Base::hasInstances()
+{
+    return instances().size() > 0;
+}
+
+std::vector<std::shared_ptr<Helper::Instance>> Base::instances()
+{
+    return _registry->impl->thread.executeOnThread<std::vector<std::shared_ptr<Helper::Instance>>>([this]() {
+        std::vector<std::shared_ptr<Helper::Instance>> vect;
+        auto instances = ubuntu_app_launch_list_helper_instances(_type.value().c_str(), ((std::string)_appid).c_str());
+        for (int i = 0; instances[i] != nullptr; i++)
+        {
+            auto inst = std::make_shared<BaseInstance>(_appid, _type, instances[i], _registry);
+            vect.push_back(inst);
+        }
+
+        g_strfreev(instances);
+
+        return vect;
+    });
 }
 
 std::shared_ptr<gchar*> urlsToStrv(std::vector<Helper::URL> urls)
@@ -159,32 +150,32 @@ std::shared_ptr<gchar*> urlsToStrv(std::vector<Helper::URL> urls)
     return std::shared_ptr<gchar*>((gchar**)g_array_free(array, FALSE), g_strfreev);
 }
 
-std::shared_ptr<Click::Instance> Click::launch(std::vector<Helper::URL> urls)
+std::shared_ptr<Helper::Instance> Base::launch(std::vector<Helper::URL> urls)
 {
     auto urlstrv = urlsToStrv(urls);
 
-    return _registry->impl->thread.executeOnThread<std::shared_ptr<Click::Instance>>([this, urlstrv]() {
+    return _registry->impl->thread.executeOnThread<std::shared_ptr<BaseInstance>>([this, urlstrv]() {
         auto instanceid = ubuntu_app_launch_start_multiple_helper(_type.value().c_str(), ((std::string)_appid).c_str(),
                                                                   urlstrv.get());
-        auto ret = std::make_shared<ClickInstance>(_appid, _type, instanceid, _registry);
+        auto ret = std::make_shared<BaseInstance>(_appid, _type, instanceid, _registry);
         g_free(instanceid);
         return ret;
     });
 }
 
-std::shared_ptr<Click::Instance> Click::launch(MirPromptSession* session, std::vector<Helper::URL> urls)
+std::shared_ptr<Helper::Instance> Base::launch(MirPromptSession* session, std::vector<Helper::URL> urls)
 {
     auto urlstrv = urlsToStrv(urls);
 
-    return _registry->impl->thread.executeOnThread<std::shared_ptr<Click::Instance>>([this, session, urlstrv]() {
+    return _registry->impl->thread.executeOnThread<std::shared_ptr<BaseInstance>>([this, session, urlstrv]() {
         auto instanceid = ubuntu_app_launch_start_session_helper(_type.value().c_str(), session,
                                                                  ((std::string)_appid).c_str(), urlstrv.get());
 
-        return std::make_shared<ClickInstance>(_appid, _type, instanceid, _registry);
+        return std::make_shared<BaseInstance>(_appid, _type, instanceid, _registry);
     });
 }
 
-std::list<std::shared_ptr<Helper>> Click::running(Helper::Type type, std::shared_ptr<Registry> registry)
+std::list<std::shared_ptr<Helper>> Base::running(Helper::Type type, std::shared_ptr<Registry> registry)
 {
     return registry->impl->thread.executeOnThread<std::list<std::shared_ptr<Helper>>>([type, registry]() {
         std::list<std::shared_ptr<Helper>> helpers;
@@ -192,7 +183,7 @@ std::list<std::shared_ptr<Helper>> Click::running(Helper::Type type, std::shared
         auto appidv = ubuntu_app_launch_list_helpers(type.value().c_str());
         for (int i = 0; appidv[i] != nullptr; i++)
         {
-            auto helper = std::make_shared<Click>(type, AppID::parse(appidv[i]), registry);
+            auto helper = std::make_shared<Base>(type, AppID::parse(appidv[i]), registry);
             helpers.push_back(helper);
         }
 
@@ -211,7 +202,7 @@ std::list<std::shared_ptr<Helper>> Click::running(Helper::Type type, std::shared
 std::shared_ptr<Helper> Helper::create(Type type, AppID appid, std::shared_ptr<Registry> registry)
 {
     /* Only one type today */
-    return std::make_shared<helper_impls::Click>(type, appid, registry);
+    return std::make_shared<helper_impls::Base>(type, appid, registry);
 }
 
 }  // namespace app_launch
