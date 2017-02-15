46241157ae2039d3266426d966a1f86b3fff1706
Ted Gould <ted@gould.cx>
2017-02-15 16:07:09 -0600

    Refactoring to be more jobs manager based

--- libubuntu-app-launch/helper.cpp
+++ libubuntu-app-launch/helper.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016 Canonical Ltd.
+ * Copyright © 2016-2017 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 3, as published
@@ -17,6 +17,7 @@
  *     Ted Gould <ted.gould@canonical.com>
  */
 
+#include <algorithm>
 #include <list>
 
 #include "helper.h"
@@ -31,53 +32,32 @@ namespace app_launch
 namespace helper_impls
 {
 
+/** We really should have put a relationship between Helper::Instance
+and Application::Instance in the API. But to work around that today
+we're just handling it here because the helper interface is a subset
+of what the Application::Instance class provides */
 class BaseInstance : public Helper::Instance
 {
-public: /* all one file, no one to hide from */
-    AppID _appid;
-    Helper::Type _type;
-    std::string _instanceid;
-    std::shared_ptr<Registry> _registry;
+public:
+    std::shared_ptr<jobs::instance::Base> impl;
 
-    BaseInstance(const AppID& appid,
-                 const Helper::Type& type,
-                 const std::string& instanceid,
-                 std::shared_ptr<Registry> registry)
-        : _appid(appid)
-        , _type(type)
-        , _instanceid(instanceid)
-        , _registry(registry)
+    BaseInstance(const std::shared_ptr<jobs::instance::Base>& inst)
+        : impl{inst}
+    {
+    }
+    BaseInstance(const std::shared_ptr<Application::Instance>& inst)
+        : impl{std::dynamic_pointer_cast<jobs::instance::Base>(inst)}
     {
     }
 
     bool isRunning() override
     {
-        return _registry->impl->thread.executeOnThread<bool>([this]() {
-            bool found = false;
-
-            auto instances =
-                ubuntu_app_launch_list_helper_instances(_type.value().c_str(), ((std::string)_appid).c_str());
-            for (int i = 0; instances[i] != nullptr; i++)
-            {
-                if (_instanceid == std::string(instances[i]))
-                {
-                    found = true;
-                    break;
-                }
-            }
-
-            g_strfreev(instances);
-
-            return found;
-        });
+        return impl->isRunning();
     }
 
     void stop() override
     {
-        _registry->impl->thread.executeOnThread<bool>([this]() {
-            return ubuntu_app_launch_stop_multiple_helper(_type.value().c_str(), ((std::string)_appid).c_str(),
-                                                          _instanceid.c_str()) == TRUE;
-        });
+        impl->stop();
     }
 };
 
@@ -102,8 +82,6 @@ public:
     std::shared_ptr<Helper::Instance> launch(std::vector<Helper::URL> urls = {}) override;
     std::shared_ptr<Helper::Instance> launch(MirPromptSession* session, std::vector<Helper::URL> urls = {}) override;
 
-    static std::list<std::shared_ptr<Helper>> running(Helper::Type type, std::shared_ptr<Registry> registry);
-
 private:
     Helper::Type _type;
     AppID _appid;
@@ -117,80 +95,43 @@ bool Base::hasInstances()
 
 std::vector<std::shared_ptr<Helper::Instance>> Base::instances()
 {
-    return _registry->impl->thread.executeOnThread<std::vector<std::shared_ptr<Helper::Instance>>>([this]() {
-        std::vector<std::shared_ptr<Helper::Instance>> vect;
-        auto instances = ubuntu_app_launch_list_helper_instances(_type.value().c_str(), ((std::string)_appid).c_str());
-        for (int i = 0; instances[i] != nullptr; i++)
-        {
-            auto inst = std::make_shared<BaseInstance>(_appid, _type, instances[i], _registry);
-            vect.push_back(inst);
-        }
+    auto insts = _registry->impl->jobs->instances(_appid, _type.value());
+    std::vector<std::shared_ptr<Helper::Instance>> wrapped;
 
-        g_strfreev(instances);
+    std::transform(insts.begin(), insts.end(), wrapped.begin(),
+                   [](std::shared_ptr<jobs::instance::Base>& inst) { return std::make_shared<BaseInstance>(inst); });
 
-        return vect;
-    });
+    return wrapped;
 }
 
-std::shared_ptr<gchar*> urlsToStrv(std::vector<Helper::URL> urls)
+std::vector<Application::URL> appURL(const std::vector<Helper::URL>& in)
 {
-    if (urls.size() == 0)
-    {
-        return {};
-    }
-
-    auto array = g_array_new(TRUE, FALSE, sizeof(gchar*));
-
-    for (auto url : urls)
-    {
-        auto str = g_strdup(url.value().c_str());
-        g_array_append_val(array, str);
-    }
-
-    return std::shared_ptr<gchar*>((gchar**)g_array_free(array, FALSE), g_strfreev);
+    std::vector<Application::URL> out;
+    std::transform(in.begin(), in.end(), out.begin(),
+                   [](Helper::URL url) { return Application::URL::from_raw(url.value()); });
+    return out;
 }
 
 std::shared_ptr<Helper::Instance> Base::launch(std::vector<Helper::URL> urls)
 {
-    auto urlstrv = urlsToStrv(urls);
+    std::function<std::list<std::pair<std::string, std::string>>()> envfunc = [this]() {
+        return std::list<std::pair<std::string, std::string>>{};
+    };
 
-    return _registry->impl->thread.executeOnThread<std::shared_ptr<BaseInstance>>([this, urlstrv]() {
-        auto instanceid = ubuntu_app_launch_start_multiple_helper(_type.value().c_str(), ((std::string)_appid).c_str(),
-                                                                  urlstrv.get());
-        auto ret = std::make_shared<BaseInstance>(_appid, _type, instanceid, _registry);
-        g_free(instanceid);
-        return ret;
-    });
+    /* TODO */
+    return std::make_shared<BaseInstance>(_registry->impl->jobs->launch(
+        _appid, _type.value(), std::string{}, appURL(urls), jobs::manager::launchMode::STANDARD, envfunc));
 }
 
 std::shared_ptr<Helper::Instance> Base::launch(MirPromptSession* session, std::vector<Helper::URL> urls)
 {
-    auto urlstrv = urlsToStrv(urls);
+    std::function<std::list<std::pair<std::string, std::string>>()> envfunc = [this]() {
+        return std::list<std::pair<std::string, std::string>>{};
+    };
 
-    return _registry->impl->thread.executeOnThread<std::shared_ptr<BaseInstance>>([this, session, urlstrv]() {
-        auto instanceid = ubuntu_app_launch_start_session_helper(_type.value().c_str(), session,
-                                                                 ((std::string)_appid).c_str(), urlstrv.get());
-
-        return std::make_shared<BaseInstance>(_appid, _type, instanceid, _registry);
-    });
-}
-
-std::list<std::shared_ptr<Helper>> Base::running(Helper::Type type, std::shared_ptr<Registry> registry)
-{
-    return registry->impl->thread.executeOnThread<std::list<std::shared_ptr<Helper>>>([type, registry]() {
-        std::list<std::shared_ptr<Helper>> helpers;
-
-        auto appidv = ubuntu_app_launch_list_helpers(type.value().c_str());
-        for (int i = 0; appidv[i] != nullptr; i++)
-        {
-            auto helper = std::make_shared<Base>(type, AppID::parse(appidv[i]), registry);
-            helpers.push_back(helper);
-        }
-
-        g_strfreev(appidv);
-
-        return helpers;
-    });
+    /* TODO */
+    return std::make_shared<BaseInstance>(_registry->impl->jobs->launch(
+        _appid, _type.value(), std::string{}, appURL(urls), jobs::manager::launchMode::STANDARD, envfunc));
 }
 
 }  // namespace helper_impl
