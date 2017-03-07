b3dfc610ff75fdc14ba0d6445718c7297396e79f
Ted Gould <ted@gould.cx>
2017-03-07 09:02:05 -0600

    Only default to XMir if there isn't a touch key set

--- libubuntu-app-launch/application-impl-legacy.cpp
+++ libubuntu-app-launch/application-impl-legacy.cpp
@@ -62,9 +62,14 @@ Legacy::Legacy(const AppID::AppName& appname, const std::shared_ptr<Registry>& r
         rootDir = rootenv;
     }
 
-    appinfo_ = std::make_shared<app_info::Desktop>(
-        appId(), _keyfile, _basedir, rootDir,
-        app_info::DesktopFlags::ALLOW_NO_DISPLAY | app_info::DesktopFlags::XMIR_DEFAULT, _registry);
+    auto flags = app_info::DesktopFlags::ALLOW_NO_DISPLAY;
+
+    if (!g_key_file_has_key(_keyfile.get(), "Desktop Entry", "X-Ubuntu-Touch", nullptr))
+    {
+        flags |= app_info::DesktopFlags::XMIR_DEFAULT;
+    }
+
+    appinfo_ = std::make_shared<app_info::Desktop>(appId(), _keyfile, _basedir, rootDir, flags, _registry);
 
     if (!_keyfile)
     {
